7090c7634c04abd72f0c6d3c5bdc8c90e0722691
clasocki <clasocki@gmail.com>
2013-11-06 13:31:39 +0100

    Windows Phone 8 - Prototype

---
+++ Wazniak/wazniak_forever/Model/SampleDataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wazniak_forever.Model
+{
+    public class SampleItem
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+        
+        public int? Number { get; set; }
+
+    }
+
+    public class SampleItemContext 
+    {
+        public IMobileServiceTable<SampleItem> Items = App.MobileService.GetTable<SampleItem>();
+    }
+}
---
+++ Wazniak/wazniak_forever/ViewModel/SampleViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wazniak_forever.Model;
+
+namespace wazniak_forever.ViewModel
+{
+    public class SampleViewModel : INotifyPropertyChanged
+    {
+        private SampleItemContext _sampleDB;
+
+        public SampleViewModel()
+        {
+            _sampleDB = new SampleItemContext();
+        }
+
+        private MobileServiceCollection<SampleItem, SampleItem> _allSampleItems;
+        public MobileServiceCollection<SampleItem, SampleItem> AllSampleItems
+        {
+            get { return _allSampleItems; }
+            set
+            {
+                _allSampleItems = value;
+                NotifyPropertyChanged("AllSampleItems");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify the app that a property has changed.
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        public async void LoadCollectionsFromDatabase()
+        {
+            AllSampleItems = await _sampleDB.Items.ToCollectionAsync();
+        }
+
+        public async void AddSampleItem(SampleItem newSampleItem)
+        {
+            await _sampleDB.Items.InsertAsync(newSampleItem);
+            AllSampleItems.Add(newSampleItem);
+
+        }
+
+        public async void DeleteSampleitem(SampleItem removedSampleitem)
+        {
+            await _sampleDB.Items.DeleteAsync(removedSampleitem);
+            AllSampleItems.Remove(removedSampleitem);
+        }
+    }
+}
