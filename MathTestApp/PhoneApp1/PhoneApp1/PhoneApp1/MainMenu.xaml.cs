5d87e486fc9d021e42d2986c3c1b7ed16c6920ef
Tomek Jurkowski <tomasz.jurkowski.inf@gmail.com>
2013-12-05 00:26:14 +0100

    MathExpressionTestApp added

---
+++ MathTestApp/PhoneApp1/PhoneApp1/PhoneApp1/MainMenu.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Collections.ObjectModel;
+
+namespace PhoneApp1
+{
+    public partial class MainMenu : PhoneApplicationPage
+    {
+        public ObservableCollection<Option> AllOptions { get; private set; }
+        public MainMenu()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            LoadOptions();
+        }
+
+        private void LoadOptions()
+        {
+            AllOptions = new ObservableCollection<Option>()
+            {
+                new Option("Start Course", "Uri"),
+                new Option("Settings", "Uri")
+            };
+        }
+    }
+}
