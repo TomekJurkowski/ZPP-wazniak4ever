a49256858639b9cab46bc99a626be0002d171ceb
Tomek Jurkowski <tomasz.jurkowski.inf@gmail.com>
2013-12-11 00:05:39 +0100

    Django web page removed

--- WazniakWebsite - Django/wazniakWeb/admin.py
+++
@@ -1,8 +0,0 @@
-from models import SampleItem
-from django.contrib import admin
-
-
-class SampleItemAdmin(admin.ModelAdmin):
-    list_display = ('text', 'number')
-
-admin.site.register(SampleItem, SampleItemAdmin)
